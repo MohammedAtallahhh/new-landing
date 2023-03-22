e9ca74216dad7665fdb5f0c87deb45acf45167d2
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-22 14:27:11 +0200

    Added newsletter and footer section (mobile)

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+new Swiper("#shops", {
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
 
+    991: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: ".shops__slider__controls",
+    clickable: true,
+  },
+});
+
+new Swiper("#sale", {
+  spaceBetween: 20,
+  slidesPerView: 2,
+  autoplay: true,
+  breakpoints: {
+    541: {
+      slidesPerView: 3,
+    },
+
+    768: {
+      slidesPerView: 4,
+    },
+
+    991: {
+      slidesPerView: "auto",
+    },
+  },
+
+  pagination: {
+    el: ".sale__slider__controls",
+    clickable: true,
+  },
+});
+
+new Swiper("#categories", {
+  spaceBetween: 20,
+  autoplay: true,
+
+  pagination: {
+    el: ".categories__slider__controls",
+    clickable: true,
+  },
+});
