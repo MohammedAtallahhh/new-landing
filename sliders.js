743ab906bb647744dba80cd3cc8bec4ded6202e7
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-23 14:00:03 +0200

    Made newsletter and footer sections responsive && activate header dropdowns

---
+++ sliders.js
@@ -0,0 +1,95 @@
+new Swiper("#categories", {
+  spaceBetween: 20,
+  autoplay: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+    },
+
+    991: {
+      slidesPerView: 5,
+    },
+
+    1200: {
+      slidesPerView: 6,
+    },
+  },
+
+  pagination: {
+    el: ".categories__slider__controls",
+    clickable: true,
+  },
+});
+
+new Swiper("#shops", {
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+
+    991: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: ".shops__slider__controls",
+    clickable: true,
+  },
+});
+
+new Swiper("#sale", {
+  spaceBetween: 20,
+  slidesPerView: 2,
+  autoplay: true,
+  breakpoints: {
+    541: {
+      slidesPerView: 3,
+    },
+
+    768: {
+      slidesPerView: 4,
+    },
+
+    991: {
+      slidesPerView: 5,
+    },
+
+    1200: {
+      slidesPerView: 6,
+    },
+  },
+
+  pagination: {
+    el: ".sale__slider__controls",
+    clickable: true,
+  },
+});
+
+new Swiper("#offers", {
+  spaceBetween: 20,
+  autoplay: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+
+    991: {
+      slidesPerView: 3,
+    },
+
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+
+  pagination: {
+    el: ".offers__slider__controls",
+    clickable: true,
+  },
+});
