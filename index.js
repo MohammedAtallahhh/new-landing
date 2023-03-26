bfc188710f1fb8ad76a786d5fbbcebf6551e4177
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-26 13:09:00 +0200

    Close dropdown on scroll and clicking outside

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const header = document.querySelector(".main__header");
 
+const closeDropdown = () => {
+  const active = document.querySelector(".dropdown .items.active");
+  active && active.classList.remove("active");
+};
+
 header.addEventListener("click", (e) => {
-  const target = e.target.closest(".language") || e.target.closest(".country");
+  const target = e.target.closest(".dropdown");
   if (!target) return;
+
+  const currentActive = document.querySelector(".dropdown .items.active");
+
+  if (currentActive && currentActive !== target.querySelector(".items")) {
+    currentActive.classList.remove("active");
+  }
 
   target.querySelector(".items").classList.toggle("active");
 });
+
+document.addEventListener("click", (e) => {
+  const target = e.target.closest(".dropdown");
+  if (target) return;
+
+  closeDropdown();
+});
+
+window.addEventListener("scroll", closeDropdown);
