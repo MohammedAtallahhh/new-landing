e2cb1366c9817b005475749e7e980f4f2b4ba782
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-26 16:58:46 +0200

    Added offers timer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const header = document.querySelector(".main__header");
+const offers = [...document.querySelectorAll("#offers .offer")];
 
 const closeDropdown = () => {
   const active = document.querySelector(".dropdown .items.active");
@@ -30,3 +31,46 @@ document.addEventListener("click", (e) => {
 });
 
 window.addEventListener("scroll", closeDropdown);
+
+// Offers counters
+const setupCounter = (target) => {
+  // Seconds of 2 DAYS
+  let timerValue = 2 * 24 * 60 * 60;
+
+  const intervalId = setInterval(() => {
+    // Calculate the days, hours, minutes, and seconds
+    const days = Math.floor(timerValue / (24 * 3600));
+    const hours = Math.floor((timerValue % (24 * 3600)) / 3600);
+    const minutes = Math.floor((timerValue % 3600) / 60);
+    const seconds = timerValue % 60;
+
+    // Update the timer display for mobile and desktop
+    target
+      .querySelectorAll(".days")
+      .forEach((e) => (e.textContent = days.toString().padStart(2, "0")));
+
+    target
+      .querySelectorAll(".hours")
+      .forEach((e) => (e.textContent = hours.toString().padStart(2, "0")));
+
+    target
+      .querySelectorAll(".minutes")
+      .forEach((e) => (e.textContent = minutes.toString().padStart(2, "0")));
+
+    target
+      .querySelectorAll(".seconds")
+      .forEach((e) => (e.textContent = seconds.toString().padStart(2, "0")));
+
+    // Decrement the timer value
+    timerValue--;
+
+    // Stop the timer when the value reaches 0
+    if (timerValue < 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+};
+
+offers.forEach((offer) => {
+  setupCounter(offer);
+});
